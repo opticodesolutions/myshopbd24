11576b7c4433e226791e11ec6814b9472356bd53
MOHAMMAD MUNAYAM SOWDAGOR <72827332+rsmmonaem@users.noreply.github.com>
2024-08-02 12:15:12 +0600

    commission update

--- app/Models/Comission.php
+++ app/Models/Comission.php
@@ -1,12 +1,9 @@
 <?php
-
 namespace App\Models;
 
 use App\Models\Product;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-
-
 
 class Commission extends Model
 {
