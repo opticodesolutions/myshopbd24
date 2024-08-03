00afda59d831001e60bf11def2541e674572468e
MOHAMMAD MUNAYAM SOWDAGOR <72827332+rsmmonaem@users.noreply.github.com>
2024-08-04 01:05:12 +0600

    Mastering Done And Composer Update

--- resources/views/super-admin/index.blade.php
+++ resources/views/super-admin/index.blade.php
@@ -1,10 +1,43 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 <head>
-    <title>Super Admin Dashboard</title>
+    @include('super-admin.includes.head')
 </head>
-<body>
-    <h1>Welcome Super Admin</h1>
-    <p>This is the Super Admin dashboard.</p>
+<body data-theme="default" data-layout="fluid" data-sidebar-position="left" data-sidebar-layout="default">
+	<div class="wrapper">
+        @include('super-admin.includes.sidebar')
+
+		<div class="main">
+
+            @include('super-admin.includes.topbar')
+
+            @yield('content')
+
+            @include('super-admin.includes.footer')
+		</div>
+	</div>
+	<script src="{{ asset('backend/js/app.js') }}"></script>
+<script>
+  document.addEventListener("DOMContentLoaded", function(event) {
+    setTimeout(function(){
+      if(localStorage.getItem('popState') !== 'shown'){
+        window.notyf.open({
+          type: "success",
+          message: "Get access to all 500+ components and 45+ pages with AdminKit PRO. <u><a class=\"text-white\" href=\"https://adminkit.io/pricing\" target=\"_blank\">More info</a></u> 🚀",
+          duration: 10000,
+          ripple: true,
+          dismissible: false,
+          position: {
+            x: "left",
+            y: "bottom"
+          }
+        });
+
+        localStorage.setItem('popState','shown');
+      }
+    }, 15000);
+  });
+</script>
 </body>
 </html>
