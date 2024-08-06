7acee2413c8fa84d3b8a425aa28f811883d33341
arafat-web <arafat.122260@gmail.com>
2024-08-06 16:15:24 +0600

    superadmin route updated

--- routes/web.php
+++ routes/web.php
@@ -21,30 +21,19 @@ Route::get('/signUp', [AuthController::class, 'signUp'])->name('signUp');
 Route::post('/signUp', [AuthController::class, 'signUpProcess'])->name('signUpProcess');
 
 
-// Product routes
-Route::resource('products', ProductController::class);
-
-// Comission routes
-Route::resource('commissions', CommissionController::class);
-
-// Brand routes
-Route::resource('brands', BrandController::class);
-
-// Category routes
-Route::resource('categories', CategoryController::class);
-
-// Media routes
-Route::resource('medias', MediaController::class);
-
-
-
-
-
-
-
 // Super Admin Routes
 Route::group(['middleware' => ['role:super-admin']], function () {
     Route::get('/super-admin', [SuperAdminController::class, 'index']);
+    // Product routes
+    Route::resource('products', ProductController::class);
+    // Comission routes
+    Route::resource('commissions', CommissionController::class);
+    // Brand routes
+    Route::resource('brands', BrandController::class);
+    // Category routes
+    Route::resource('categories', CategoryController::class);
+    // Media routes
+    Route::resource('medias', MediaController::class);
 });
 
 // Admin Routes
