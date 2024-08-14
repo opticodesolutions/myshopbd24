ba60c1cf39070dcebb4deb0e6ba59dde910f63bb
MOHAMMAD MUNAYAM SOWDAGOR <72827332+rsmmonaem@users.noreply.github.com>
2024-08-14 12:11:03 +0600

    All User Income trx

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
+use App\Models\Transaction;
+use App\Models\Sale;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,6 +12,50 @@ class UserController extends Controller
 {
     public function index()
     {
-        return view('user.home.index');
+        $user = auth()->user();
+        $customer = Customer::where('user_id', $user->id)->first();
+
+        // Total Count referred customers
+        $Total_reffers = Customer::where('refer_by', $customer->refer_code)->count();
+
+        //Balance Customer
+        $Balance_customer = $customer->wallet_balance;
+
+        // Total withdrown amount
+        $Total_withdrawn_amount = Transaction::where('transaction_type', 'withdraw')->where('user_id', $user->id)->sum('amount');
+
+        // Total Topup amount
+        $Total_topup_amount = Transaction::where('transaction_type', 'topup')->where('user_id', $user->id)->sum('amount');
+
+        // Total transactions amount sum
+        $Total_transactions_amount = Transaction::where('user_id', $user->id)->sum('amount');
+
+        // Total Count transactions
+        $Total_commission_transactions = Transaction::where('transaction_type', 'commission')->where('user_id', $user->id)->count();
+
+        // Total Count transactions
+        $Total_commission_transaction = Transaction::where('transaction_type', 'commission')->where('user_id', $user->id)->sum('amount');
+        // Total earnings
+       // $Total_earnings = $Total_transactions_amount - $Total_withdrawn_amount;
+
+        // Total Count sales
+        $Total_sells = Sale::where('user_id', $user->id)->count();
+
+        // Total Sales Commission Sum
+        $Total_sells_commission = Sale::where('user_id', $user->id)->sum('commission');
+
+        return view('user.home.index', compact(
+            'Total_reffers',
+            'Total_transactions_amount',
+            'Total_commission_transactions',
+            'Total_commission_transaction',
+            // 'Total_earnings',
+            'Total_sells',
+            'Total_sells_commission',
+            'Balance_customer',
+            'Total_withdrawn_amount',
+            'Total_topup_amount'
+        ));
     }
+
 }
--- resources/views/user/home/index.blade.php
+++ resources/views/user/home/index.blade.php
@@ -1,104 +1,209 @@
 @extends('layout.app')
 @section('title', 'Home')
 @section('content')
-    <main class="content">
-        <div class="container-fluid p-0">
-            <div class="row mb-2 mb-xl-3">
-                <div class="col-auto d-none d-sm-block">
-                    <h3> Dashboard</h3>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-6 col-xl-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col mt-0">
-                                    <h5 class="card-title">Income</h5>
-                                </div>
-
-                                <div class="col-auto">
-                                    <div class="stat text-primary">
-                                        <i class="align-middle" data-feather="dollar-sign"></i>
-                                    </div>
-                                </div>
-                            </div>
-                            <h1 class="mt-1 mb-3">$47.482</h1>
-                            <div class="mb-0">
-                                <span class="badge badge-success-light">3.65%</span>
-                                <span class="text-muted">Since last week</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-xl-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col mt-0">
-                                    <h5 class="card-title">Orders</h5>
-                                </div>
-
-                                <div class="col-auto">
-                                    <div class="stat text-primary">
-                                        <i class="align-middle" data-feather="shopping-bag"></i>
-                                    </div>
-                                </div>
-                            </div>
-                            <h1 class="mt-1 mb-3">2.542</h1>
-                            <div class="mb-0">
-                                <span class="badge badge-danger-light">-5.25%</span>
-                                <span class="text-muted">Since last week</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-xl-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col mt-0">
-                                    <h5 class="card-title">Activity</h5>
-                                </div>
-
-                                <div class="col-auto">
-                                    <div class="stat text-primary">
-                                        <i class="align-middle" data-feather="activity"></i>
-                                    </div>
-                                </div>
-                            </div>
-                            <h1 class="mt-1 mb-3">16.300</h1>
-                            <div class="mb-0">
-                                <span class="badge badge-success-light">4.65%</span>
-                                <span class="text-muted">Since last week</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-xl-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col mt-0">
-                                    <h5 class="card-title">Revenue</h5>
-                                </div>
-
-                                <div class="col-auto">
-                                    <div class="stat text-primary">
-                                        <i class="align-middle" data-feather="shopping-cart"></i>
-                                    </div>
-                                </div>
-                            </div>
-                            <h1 class="mt-1 mb-3">$20.120</h1>
-                            <div class="mb-0">
-                                <span class="badge badge-success-light">2.35%</span>
-                                <span class="text-muted">Since last week</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+<main class="content">
+    <div class="container-fluid p-0">
+        <div class="row mb-2 mb-xl-3">
+            <div class="col-auto d-none d-sm-block">
+                <h3>Dashboard</h3>
             </div>
         </div>
-    </main>
+        <div class="row">
+            <div class="col-sm-6 col-xl-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col mt-0">
+                                <h5 class="card-title">Income</h5>
+                            </div>
+                            <div class="col-auto">
+                                <div class="stat text-primary">
+                                    <i class="align-middle" data-feather="dollar-sign"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <h1 class="mt-1 mb-3">{{ number_format($Total_transactions_amount-$Total_withdrawn_amount, 2) }}</h1>
+                        <div class="mb-0">
+                            <span class="badge badge-success-light">3.65%</span>
+                            <span class="text-muted">Since last week</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col mt-0">
+                                <h5 class="card-title">Orders</h5>
+                            </div>
+                            <div class="col-auto">
+                                <div class="stat text-primary">
+                                    <i class="align-middle" data-feather="shopping-bag"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <h1 class="mt-1 mb-3">{{ $Total_sells }}</h1>
+                        <div class="mb-0">
+                            <span class="badge badge-danger-light">-5.25%</span>
+                            <span class="text-muted">Since last week</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col mt-0">
+                                <h5 class="card-title">Total Commission's</h5>
+                            </div>
+                            <div class="col-auto">
+                                <div class="stat text-primary">
+                                    <i class="align-middle" data-feather="activity"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <h1 class="mt-1 mb-3">{{ $Total_commission_transactions }}</h1>
+                        <div class="mb-0">
+                            <span class="badge badge-success-light">4.65%</span>
+                            <span class="text-muted">Since last week</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col mt-0">
+                                <h5 class="card-title">Total Commission</h5>
+                            </div>
+                            <div class="col-auto">
+                                <div class="stat text-primary">
+                                    <i class="align-middle" data-feather="shopping-cart"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <h1 class="mt-1 mb-3">{{ number_format($Total_sells_commission, 2) }}</h1>
+                        <div class="mb-0">
+                            <span class="badge badge-success-light">2.35%</span>
+                            <span class="text-muted">Since last week</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col mt-0">
+                                <h5 class="card-title">Total Commission Balance</h5>
+                            </div>
+                            <div class="col-auto">
+                                <div class="stat text-primary">
+                                    <i class="align-middle" data-feather="dollar-sign"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <h1 class="mt-1 mb-3">{{ number_format($Total_commission_transaction, 2) }}</h1>
+                        <div class="mb-0">
+                            <span class="badge badge-success-light">3.65%</span>
+                            <span class="text-muted">Since last week</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col mt-0">
+                                <h5 class="card-title">Total Withdraw </h5>
+                            </div>
+                            <div class="col-auto">
+                                <div class="stat text-primary">
+                                    <i class="align-middle" data-feather="dollar-sign"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <h1 class="mt-1 mb-3">{{ number_format($Total_withdrawn_amount, 2) }}</h1>
+                        <div class="mb-0">
+                            <span class="badge badge-success-light">3.65%</span>
+                            <span class="text-muted">Since last week</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col mt-0">
+                                <h5 class="card-title">Total Topup</h5>
+                            </div>
+                            <div class="col-auto">
+                                <div class="stat text-primary">
+                                    <i class="align-middle" data-feather="dollar-sign"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <h1 class="mt-1 mb-3">{{ number_format($Total_topup_amount, 2) }}</h1>
+                        <div class="mb-0">
+                            <span class="badge badge-success-light">3.65%</span>
+                            <span class="text-muted">Since last week</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xl-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col mt-0">
+                                <h5 class="card-title">Balance</h5>
+                            </div>
+                            <div class="col-auto">
+                                <div class="stat text-primary">
+                                    <i class="align-middle" data-feather="dollar-sign"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <h1 class="mt-1 mb-3">{{ number_format($Balance_customer, 2) }}</h1>
+                        <div class="mb-0">
+                            <span class="badge badge-success-light">3.65%</span>
+                            <span class="text-muted">Since last week</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-xl-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col mt-0">
+                                <h5 class="card-title">All Transations </h5>
+                            </div>
+                            <div class="col-auto">
+                                <div class="stat text-primary">
+                                    <i class="align-middle" data-feather="dollar-sign"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <h1 class="mt-1 mb-3">{{ number_format($Total_transactions_amount, 2) }}</h1>
+                        <div class="mb-0">
+                            <span class="badge badge-success-light">3.65%</span>
+                            <span class="text-muted">Since last week</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</main>
 
 @endsection
